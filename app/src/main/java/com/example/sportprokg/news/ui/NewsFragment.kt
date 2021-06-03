40557d33e5825a96beed91d7531e8de25d7f3658
kalysbek-z <kalysbek_z@auca.kg>
2021-06-03 22:00:04 +0600

    news card, recyclerview

--- app/src/main/java/com/example/sportprokg/news/ui/NewsFragment.kt
+++ app/src/main/java/com/example/sportprokg/news/ui/NewsFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.*
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sportprokg.R
+import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mAdapter: NewsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,21 @@ class NewsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_news, container, false)
         val toolbar = view.findViewById<Toolbar>(R.id.news_toolbar)
         toolbar.inflateMenu(R.menu.news_toolbar_menu)
+
+        //dummy data for recycler
+//        val list = ArrayList<NewsItem>()
+//
+//        val img = R.drawable.news_sample_img
+//        val sport = "Баскетбол"
+//        val title = "Баер снизит стоимость Кая Хаверца"
+//        val date = "03.06.2021"
+//        var it = NewsItem(img, sport, title, date)
+//        list.add(it)
+//
+//        recyclerView = view.findViewById<RecyclerView>(R.id.news_recyclerview)
+//        mAdapter = NewsAdapter(list)
+//        recyclerView.adapter = mAdapter
+//        recyclerView.layoutManager = LinearLayoutManager(context)
 
         return view
     }
